3b01d70a33714311596b50140e3bc58935d02289
Saidgaffor Sabyrov <sabyrov.saidgaffor@gmail.com>
2021-09-14 23:10:33 +0600

    Fixed maximum-subarray solution error

--- src/maximum_subarray/Solution.java
+++ src/maximum_subarray/Solution.java
@@ -1,7 +1,9 @@
+package maximum_subarray;
+
 public class Solution {
 
     public static void main(String[] args){
-        int[] testArray = {-4, -1, 2, 8, -5, 1, 9, -10, -5, 7};
+        int[] testArray = {-3,-2,0,-1};
 
         System.out.println("Maximum subarray sum: " + maximumSubarray(testArray));
     }
@@ -12,8 +14,8 @@ public class Solution {
 
         for(int i = 0; i < arr.length; ++i){
             max_ending += arr[i];
+            if(max_ending > max_so_far) max_so_far = max_ending;
             if(max_ending < 0) max_ending = 0;
-            else if(max_ending > max_so_far) max_so_far = max_ending;
         }
 
         return max_so_far;
